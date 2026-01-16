c9f81ee992938f7379ea39249f0d6087c7314e22
kwonboryong <kwonboryong@naver.com>
2026-01-16 23:13:43 +0900

    chore: react-hot-toast 설치 및 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./app/styles/global.css";
 import App from "./App.tsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Toaster
+      position="top-center"
+      toastOptions={{
+        style: {
+          fontSize: "14px",
+        },
+      }}
+    />
   </StrictMode>
 );
