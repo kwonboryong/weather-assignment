bcd595feaf0378feae415f3358c6535d4d8269eb
kwonboryong <kwonboryong@naver.com>
2026-01-17 20:33:05 +0900

    refactor: 위치 관련 api 함수와 쿼리 파일 위치 변경

---
+++ src/entities/location/model/useForwardGeocodeQuery.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { forwardGeocode } from "@/entities/location/model/geocodingApi";
+
+export function useForwardGeocode(placeFull: string | undefined) {
+  return useQuery({
+    queryKey: ["forward-geocode", placeFull],
+    queryFn: () => forwardGeocode(placeFull ?? ""),
+    enabled: !!placeFull,
+    staleTime: 1000 * 60 * 60,
+    refetchOnWindowFocus: false,
+  });
+}
